939e629ed6d8df9f1ab0f3c60afc0ceeb1b7dad4
Kevin-129 <E1398769@u.nus.edu>
2026-01-23 14:33:50 +0800

    Level 3. Mark as Done

--- src/main/java/CowPay.java
+++ src/main/java/CowPay.java
@@ -1,17 +1,16 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class CowPay {
 
-    public static final String name = " CowPay";
+    public static final String name = "CowPay";
     private static final String LINE = "\t____________________________________________________________"; 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println(LINE);
-        System.out.println("\tI'm " + name + "! Nice to meet you!");
+        System.out.println("\tHi baby, I'm " + name + "! Nice to meet you!");
         System.out.println("\tWat u wan me do?");
         System.out.println();
 
@@ -20,7 +19,8 @@ public class CowPay {
                 break; // Exit if no more input
             }
 
-            String next = scanner.nextLine().trim();
+            String inputStrings[] = scanner.nextLine().trim().split(" ");
+            String next = inputStrings[0];
 
             if(next.equals("bye")) {
                 System.out.println(LINE);
@@ -31,17 +31,56 @@ public class CowPay {
             } else if (next.equals("list")) {
                 //List all tasks
                 System.out.println(LINE);
-                System.out.println("\tOk I will do all these: ");
+                System.out.println("\tYou need do these: ");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("\t" + (i + 1) + ". " + tasks.get(i));
+                    Task t = tasks.get(i);
+                    System.out.println("\t" + (i + 1) + "." + t.getStatusIcon() + " " + t.getDescription());
                 }
                 System.out.println(LINE);
 
+            } else if (next.equals("mark") || next.equals("unmark")) {
+                //Mark or unmark task
+                if (inputStrings.length < 2) {
+                    //Check if valid task number is given
+                    System.out.println(LINE);
+                    System.out.println("\tGive a valid task number.");
+                    System.out.println(LINE);
+                    continue;
+
+                } else {
+                    int taskNum = Integer.parseInt(inputStrings[1]) - 1;
+                    if (taskNum < 0 || taskNum >= tasks.size()) {
+                        System.out.println(LINE);
+                        System.out.println("\tGive a valid task number.");
+                        System.out.println(LINE);
+                        continue;
+
+                    } else {
+                        Task t = tasks.get(taskNum);
+                        if (next.equals("mark")) {
+                            t.markAsDone();
+                            System.out.println(LINE);
+                            System.out.println("\tOk this one marked as done: ");
+                            System.out.println("\t  " + t.getStatusIcon() + " " + t.getDescription());
+                            System.out.println(LINE);
+                            
+                        } else {
+                            t.markAsNotDone();
+                            System.out.println(LINE);
+                            System.out.println("\tOk this one marked as not done: ");
+                            System.out.println("\t  " + t.getStatusIcon() + " " + t.getDescription());
+                            System.out.println(LINE);
+                        }
+                    }
+                    
+                }
+                
             } else {
                 //Add task to tasks
                 System.out.println(LINE);
-                tasks.add(next);
-                System.out.println("\tOk I will: " + next);
+                Task t = new Task(next);
+                tasks.add(t);
+                System.out.println("\tOk you need to: " + next);
                 System.out.println(LINE);
             }
 
