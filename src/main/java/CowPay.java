a6a0f0121837e32dbdf0c294b7a16f31894c1366
Kevin-129 <E1398769@u.nus.edu>
2026-01-23 13:57:42 +0800

    Level 2. Add, List

--- src/main/java/CowPay.java
+++ src/main/java/CowPay.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class CowPay {
 
@@ -7,27 +8,40 @@ public class CowPay {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println(LINE);
         System.out.println("\tI'm " + name + "! Nice to meet you!");
-        System.out.println("\tSay something...");
+        System.out.println("\tWat u wan me do?");
         System.out.println();
 
         while (true) {
             if (!scanner.hasNextLine()) {
-                break; // Exit if no more inputhi
+                break; // Exit if no more input
             }
 
             String next = scanner.nextLine().trim();
 
             if(next.equals("bye")) {
                 System.out.println(LINE);
-                System.out.println("\tBye! Don't come back!");
+                System.out.println("\tBye!! Don't come back!");
                 System.out.println(LINE);
                 break; // Exit
-            } else {
+
+            } else if (next.equals("list")) {
+                //List all tasks
                 System.out.println(LINE);
-                System.out.println("\t"+ next);
+                System.out.println("\tOk I will do all these: ");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(LINE);
+
+            } else {
+                //Add task to tasks
+                System.out.println(LINE);
+                tasks.add(next);
+                System.out.println("\tOk I will: " + next);
                 System.out.println(LINE);
             }
 
