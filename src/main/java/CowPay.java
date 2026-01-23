a70c797736284a53ea4e604d3b4947b0d1f8e9cf
Kevin-129 <E1398769@u.nus.edu>
2026-01-23 13:38:22 +0800

    Level 1. Echo

--- src/main/java/CowPay.java
+++ src/main/java/CowPay.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
+
 public class CowPay {
+
+    public static final String name = " CowPay";
+    private static final String LINE = "\t____________________________________________________________"; 
     public static void main(String[] args) {
-        String name = " CowPay";
-        
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm " + name);
-        System.out.println(" What can I do for you?");
 
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println(LINE);
+        System.out.println("\tI'm " + name + "! Nice to meet you!");
+        System.out.println("\tSay something...");
+        System.out.println();
+
+        while (true) {
+            if (!scanner.hasNextLine()) {
+                break; // Exit if no more inputhi
+            }
+
+            String next = scanner.nextLine().trim();
+
+            if(next.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println("\tBye! Don't come back!");
+                System.out.println(LINE);
+                break; // Exit
+            } else {
+                System.out.println(LINE);
+                System.out.println("\t"+ next);
+                System.out.println(LINE);
+            }
+
+        }
+
+        scanner.close();
     }
 }
