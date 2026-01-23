61768cf7aeaba9ca5e00fb10f840d315ce8ee8e2
Kevin-129 <E1398769@u.nus.edu>
2026-01-23 13:12:29 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/CowPay.java
@@ -0,0 +1,14 @@
+public class CowPay {
+    public static void main(String[] args) {
+        String name = " CowPay";
+        
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + name);
+        System.out.println(" What can I do for you?");
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
