f6cf4d07466e31e2bc287f128433a1c23596c5fb
Kevin-129 <E1398769@u.nus.edu>
2026-01-29 01:46:02 +0800

    Implement Level 7. Save

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,14 +2,23 @@ public class Event extends Task {
     protected String from;
     protected String to;
 
+    /**
+     * Creates a new event task with given description, from, to.
+     * Task is initialised as not done.
+     *
+     * @param description Description of task.
+     * @param from Start time of event.
+     * @param to End time of event.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
-    
-    public String getFromAndTo() {
-        return " (from: " + this.from + " to: " + this.to + ")";
+    public String getFrom() {
+        return this.from;
     }
-    
+    public String getTo() {
+        return this.to;
+    }
 }
