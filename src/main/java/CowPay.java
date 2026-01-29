41270ff5735c8886825536a599964d06e6213d55
Kevin-129 <E1398769@u.nus.edu>
2026-01-30 02:19:59 +0800

    Create Parser class

--- src/main/java/CowPay.java
+++ src/main/java/CowPay.java
@@ -29,15 +29,16 @@ public class CowPay {
             }
             String[] inputStrings = scanner.nextLine().trim().split(" ", 2);
             //Split on first space only
-            String next = inputStrings[0];
+            String command = inputStrings[0].toLowerCase();
+            String details = inputStrings.length < 2 ? "" : inputStrings[1].trim();
 
             ui.showLine();
 
-            if (next.equals("bye")) {
+            if (command.equals("bye")) {
                 ui.showBye();
                 break; // Exit
 
-            } else if (next.equals("list")) {
+            } else if (command.equals("list")) {
                 //List all tasks
                 System.out.println("\tYou need do these: ");
 
@@ -62,12 +63,12 @@ public class CowPay {
                     System.out.print(t.getStatusIcon() + " ");
                     System.out.println(t.getDescription() + time);
                     // E.g.
-                    // 1.[T][ ] taskDescription
-                    // 2.[D][X] taskDescription (by: time)
-                    // 3.[E][ ] taskDescription (from: time1 to: time2)
+                    // 1.[T][X] SHOWERRR
+                    // 2.[D][X] submit file (by: Jan 29 2026 23:59)
+                    // 3.[E][ ] SLEEEEEP (from: Jan 28 2026 23:59 to: Jan 29 2026 23:59)
                 }
 
-            } else if (next.equals("mark") || next.equals("unmark")) {
+            } else if (command.equals("mark") || command.equals("unmark")) {
                 //Mark or unmark task
                 Task t = null;
                 try {
@@ -84,7 +85,7 @@ public class CowPay {
                     ui.showLine();
                     continue;
                 }
-                if (next.equals("mark")) {
+                if (command.equals("mark")) {
                     t.markAsDone();
                     storage.save(tasks.asArrayList());
                     System.out.println("\tOk, this one marked as done: ");
@@ -95,72 +96,53 @@ public class CowPay {
                 }
                 System.out.println("\t  " + t.getStatusIcon() + " " + t.getDescription());
 
-            } else if (next.equals("event")) {
-                //Event task
-                String description = "";
-                String from = "";
-                String to = "";
+            } else if (command.equals("event")) {
+                Event event = null;
 
-                //Second part of input split into further parts
                 try {
-                    //HANDLE MISSING PARAMS, INVALID FORMAT
-                    String[] split1 = inputStrings[1].trim().split(" /from ", 2);
-                    String[] split2 = split1[1].trim().split(" /to ", 2);
-                    description = split1[0];
-                    from = split2[0];
-                    to = split2[1];
+                    event = Parser.parseEvent(details);
                 } catch (Exception e) {
-                    System.out.println("\tAn event has a description, /from and /to.");
-                    System.out.println("\tE.g. Enter: event SLEEEEEP /from 28/1/2026 2359 /to 29/1/2026 2359");
-                    System.out.println("\tUse the exact format! - \"/from\" and \"/to\"");
+                    System.out.println(e.getMessage());
                     ui.showLine();
                     continue;
                 }
 
-                Event e = new Event(description, from, to);
-                tasks.add(e);
+                tasks.add(event);
                 storage.save(tasks.asArrayList());
 
                 System.out.println("\tOk, you need to: ");
-                System.out.println("\t  [E][ ] " + description + " (from: " + e.getFrom() + " to: " + e.getTo() + ")");
+                System.out.println("\t  [E][ ] " + event.getDescription()
+                    + " (from: " + event.getFrom() + " to: " + event.getTo() + ")");
 
                 System.out.println("\tYou have " + tasks.size() + " tasks.");
 
-            } else if (next.equals("deadline")) {
-                //Deadline task
-                String description = "";
-                String by = "";
+            } else if (command.equals("deadline")) {
+                Deadline d = null;
 
-                //Second part of input split into further parts
                 try {
-                    //HANDLE MISSING PARAMS, INVALID FORMAT
-                    String[] split = inputStrings[1].trim().split(" /by ", 2);
-                    description = split[0];
-                    by = split[1];
-                } catch (Exception e) {
-                    System.out.println("\tAn deadline has a description, /by.");
-                    System.out.println("\tE.g. Enter: deadline submit file /by 29/1/2026 2359");
-                    System.out.println("\tUse the exact format! - \"/by\"");
+                    d = Parser.parseDeadline(details);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
                     ui.showLine();
                     continue;
                 }
-                Deadline d = new Deadline(description, by);
+
                 tasks.add(d);
                 storage.save(tasks.asArrayList());
 
                 System.out.println("\tOk, you need to: ");
-                System.out.println("\t  [D][ ] " + description + " (by: " + d.getBy() + ")");
+                System.out.println("\t  [D][ ] " + d.getDescription() + " (by: " + d.getBy() + ")");
 
                 System.out.println("\tYou have " + tasks.size() + " tasks.");
 
-            } else if (next.equals("todo")) {
+            } else if (command.equals("todo")) {
                 //Todo task
-                if (inputStrings.length < 2) {
+                if (details.isEmpty()) {
                     //HANDLE MISSING PARAMS
                     System.out.println("\tA todo has a description.");
                     System.out.println("\tE.g. Enter: todo SHOWERRR");
                 } else {
-                    String description = inputStrings[1];
+                    String description = details;
                     Task t = new Task(description);
                     tasks.add(t);
                     storage.save(tasks.asArrayList());
@@ -171,7 +153,7 @@ public class CowPay {
                     System.out.println("\tYou have " + tasks.size() + " tasks.");
                 }
 
-            } else if (next.equals("delete")) {
+            } else if (command.equals("delete")) {
 
                 //Delete task
                 Task t = null;
